0eb121cf820f11ebfd81fddeafe4b6e202094a15
Nayeem <mohammednayeem379@gmail.com>
2025-03-04 19:47:20 +0530

    editing meesho

---
+++ Tasks/Task-un/JS/script.js
@@ -0,0 +1,10 @@
+//valid Identifiers
+let myVariable = 10;  //camel case naming 
+const _hiddenVariable = 5;  //underscore prefix
+var $price = 100;  //dollar sign prefix
+console.log(myVariable, _hiddenVariable, $price);
+
+//invalid Identifiers
+// let 1stVariable = 10;  //cannot start with a number
+// const hidden-Variable = 5;  //cannot use hyphen
+// var %price = 100;  //cannot use special characters
