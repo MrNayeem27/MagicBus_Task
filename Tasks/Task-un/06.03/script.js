16e6beb6339d5e999595b56df19e2e58ee03f271
Nayeem <mohammednayeem379@gmail.com>
2025-03-06 12:42:24 +0530

    js

--- Tasks/Task-un/06.03/script.js
+++ Tasks/Task-un/06.03/script.js
@@ -1,3 +1,4 @@
+// 1.String
 // Length Method
 
 let sample= "Nayeem";
@@ -49,3 +50,36 @@ console.log(sample7.toLowerCase());
 
 let sample8= "    Nayeem    ";
 console.log(sample8.trim());
+
+
+// 2.Numbers
+// Integers & Decimals
+
+let age=25; //Integer
+let price=99.9; //Decimal
+let bigNumber=9007199254740991n; //BigInt (for larger numbers)
+console.log{typeof bigNumber}
+
+
+// 3.Boolean
+// True or False
+
+let isJavaScriptFun=true;
+let isSkyGreen=false;
+console.log(typeof isJavaScriptFun); //output boolean value
+
+
+// 4.Undefined
+let x; //undefined
+console.log (x); //output: undefined
+console.log(type of x); //output: undefined
+
+// 5.Null
+let y=null
+console.log(y); //null
+console.log(type of y); //object is a bug in JavaScript
+
+// 6.Symbol
+let id1=($Nayeem);
+let id2=($Nayeem);
+console.log(id1===id2); //false
