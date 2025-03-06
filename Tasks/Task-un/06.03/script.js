2e4dd1701f6aa9801bcb054964269ccd0554b98b
Nayeem <mohammednayeem379@gmail.com>
2025-03-06 11:00:15 +0530

    06.03.25-JS

---
+++ Tasks/Task-un/06.03/script.js
@@ -0,0 +1,51 @@
+// Length Method
+
+let sample= "Nayeem";
+console.log(sample);
+
+let test=sample.length;
+console.log(test);
+
+// Index Method
+
+let sample1= "Nayeem";
+let test1=sample1[0];
+console.log(test1);
+
+// charAT Method
+
+let sample2= "Nayeem";
+let test2=sample2.charAt(0);
+console.log(test2);
+
+// Concat Method
+
+let sample3= "Mohamed";
+let sample4= "Nayeem";
+
+console.log(sample3.concat(sample4));
+console.log(sample3.concat(" ",sample4));
+console.log (sample3+sample4);
+
+// Includes Method
+
+let sample5= "Mohamed Nayeem";
+console.log(sample5.includes("n"));
+
+let email="mohammednayeem379@gmail.com";
+console.log(email.includes("@"));
+
+// toUpperCase
+
+let sample6= "mohamed";
+console.log(sample6.toUpperCase());
+
+// toLowerCase
+
+let sample7= "NAYEEM";
+console.log(sample7.toLowerCase());
+
+// trim Method
+
+let sample8= "    Nayeem    ";
+console.log(sample8.trim());
