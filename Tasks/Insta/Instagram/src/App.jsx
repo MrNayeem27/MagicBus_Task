955245369577a2c00bcdfc2c71d178bc0505ca25
Nayeem <mohammednayeem379@gmail.com>
2025-04-02 11:40:12 +0530

    form validation

---
+++ Tasks/Insta/Instagram/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Components/NavBar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h2>Wecome to Mr.N's CoreZone</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ Tasks/Insta/Instagram/src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+          <li>Home</li>
+          <li>Search</li>
+          <li>Explore</li>
+          <li>Reels</li>
+          <li>Messages</li>
+          <li>Notification</li>
+          <li>Create</li>
+          <li>Dashboard</li>
+          <li>Profile</li>
+        </ul>
+        <ul>
+          <li>AI Studio</li>
+          <li>Threads</li>
+          <li>More</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
